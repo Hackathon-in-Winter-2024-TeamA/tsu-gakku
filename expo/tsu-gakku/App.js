ac60d79e68611973bb666271eb89a75bec4212f4
sudatema <dy.ren.ray@gmail.com>
2024-02-27 21:44:24 +0900

    tailwind test

--- expo/tsu-gakku/App.js
+++ expo/tsu-gakku/App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc';
+
+import AttendanceList from './src/components/AttendanceList';
 
 export default function App() {
     return (
         <View style={styles.container}>
             <Text>Open up App.js to start working on your app!we did it!!!</Text>
+            <Text style={tw`text-xl text-red-600`}>Hello World</Text>
             <StatusBar style="auto" />
+            <AttendanceList />
         </View>
     );
 }
---
+++ expo/tsu-gakku/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+        colors: {
+            'brand-color': '#FEDA30',
+            'second-color': '#FFF8D7',
+            'black': '#0F0F0F',
+            'gray': '#4A4A4A',
+            'light-gray': '#BCB7B7',
+        },
+        // fontSize: {
+        //     sm: '0.8rem',
+        //     base: '1rem',
+        //     xl: '1.25rem',
+        //     '2xl': '1.563rem',
+        //     '3xl': '1.953rem',
+        //     '4xl': '2.441rem',
+        //     '5xl': '3.052rem',
+        // },
+    },
+}
